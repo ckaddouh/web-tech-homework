c897b384bcce3e4cc03bb7ca3e1102bb6bf13ef5
Christina Kaddouh <christinakaddouh@gmail.com>
2026-02-16 10:54:13 +0100

    output updates upon input

---
+++ markdown-editor/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: ''
+        };
+    }, 
+    computed: {
+        htmlOutput() {
+            return marked.parse(this.message);
+        },
+        charCount() {
+            return this.htmlOutput.replace(/<[^>]*>/g, '').length;
+        }
+    }
+});
+
+
+
+app.mount('#app');
